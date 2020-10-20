b16cf2a6360db2d5d6bd6d227586e30deb7a6fff
805702 <tchakoumill@gmail.com>
2020-10-20 13:00:20 +0200

    added some transactions

--- handlers/demand.js
+++ handlers/demand.js
@@ -3,33 +3,79 @@ const { Transaction } = require('sequelize')
 const Sequelize = require('sequelize')
 
 
-verifyUserExist =async (phone,t)=>{
+verifyUserExist =async (iden,t)=>{
     try{
+        const userId = ''
         const user = await db.sequelize
-        .query(`select * from user where phoneUser = ${phone}`,{type:db.sequelize.QueryTypes.SELECT, transaction:t})
+        .query(`select * from user where phoneUser = ${iden.phone}`,{type:db.sequelize.QueryTypes.SELECT, transaction:t})
         .then(result=>result)
         
-        if (user.length===1)return true
-        else false
+        if (user.length===1)return 'found user with phone'
+        else {
+            return await createUser(iden, t)
+        }
     }catch(err){throw new Error(err)}
     
+}
+
+createUser=async(iden,t)=>{
+    try{
+        const newUser= await db.sequelize
+        .query(`insert into user
+            (
+                phoneUser,
+                firstNameUser,
+                dateOfBirthUser,
+                genderUser,
+                roleUser,
+                lastNameUser,
+                idTown,
+                emailUser
+            )
+             values(
+                ${iden.phone},
+                ${iden.fname},
+                ${iden.dob},
+                ${iden.gender.toLowerCase()},
+                ${"'patient'"},
+                ${iden.lname},
+                ${1},
+                ${iden.email?iden.email:null}
+            )`,{
+                type:db.sequelize.QueryTypes.INSERT,
+                transaction:t
+            }
+        ).then(result=>result)
+
+        console.log(newUser)
+        return newUser
+    }catch(err){throw new Error(err)}
 }
 
 exports.createTextDemand=async(req, res, next)=>{
     try{
         const t = await db.sequelize.transaction();
         // const {
-        //     choosenExam,
-        //     payingPhone,
-        //     payingService,
-        //     identification,
-        //     medPersonnel,
-        //     entryMethod
+        //     choosenExam[array of id's]<[13,16]>,
+        //     payingPhone<Number> 657140183,
+        //     payingService"String" "OM",
+        //     medPersonnel:{name:'', title:''}
+        //     entryMethod: "we already know that it is text"
         // }=req.body
-        await verifyUserExist(req.body.phone,t)
+
+        const {
+            choosenExam,
+            payingPhone,
+            payingService,
+            identification,
+            medPersonnel,
+            entryMethod
+        } = req.body
+
+        const userExist = await verifyUserExist(identification,t)
         t.commit()
         t.afterCommit(()=>{
-            res.send('hello world')
+            res.send(userExist)
         })
     }catch(err){return next(err)}
 }
