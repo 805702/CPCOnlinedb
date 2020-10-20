85c7c89808936ebcd0fd4de436b2c1f3f1ad3267
805702 <tchakoumill@gmail.com>
2020-10-20 14:46:08 +0200

    working on the transaction added medPersonnel to it

--- handlers/demand.js
+++ handlers/demand.js
@@ -3,19 +3,48 @@ const { Transaction } = require('sequelize')
 const Sequelize = require('sequelize')
 
 
-verifyUserExist =async (iden,t)=>{
+dbUser =async (iden, t)=>{
     try{
-        const userId = ''
-        const user = await db.sequelize
+        let user = await db.sequelize
         .query(`select * from user where phoneUser = ${iden.phone}`,{type:db.sequelize.QueryTypes.SELECT, transaction:t})
         .then(result=>result)
-        
-        if (user.length===1)return 'found user with phone'
-        else {
-            return await createUser(iden, t)
-        }
+
+        if (user.length===1) return user[0].idUser
+        else return await createUser(iden, t)
+
+        return userId
     }catch(err){throw new Error(err)}
     
+}
+
+dbMedPersonnel = async(med, t)=>{
+    try{
+        let medP = await db.sequelize
+        .query(`select * from MedicalPersonnel where title=${med.title} and name=${med.name}`,{
+            type:db.sequelize.QueryTypes.SELECT,
+            transaction:t
+        })
+        .then(result=>result)
+
+        if(medP.length!==1)return medP[0].idMedicalPersonnel
+        else return await createMedPersonnel(med, t)
+    }catch(err){throw new Error(err)}
+}
+
+createMedPersonnel =async(med, t)=>{
+    try{
+        const newMed = await db.sequelize.query(`
+            insert into MedicalPersonnel (title, name) 
+            values(
+                '${med.title}',
+                '${med.name}',
+            )
+        `,{
+            type: db.sequelize.QueryTypes.INSERT,
+            transaction: t
+        }).then(result=>result[0])
+        return newMed
+    }catch(err){throw new Error(err)}
 }
 
 createUser=async(iden,t)=>{
@@ -34,22 +63,26 @@ createUser=async(iden,t)=>{
             )
              values(
                 ${iden.phone},
-                ${iden.fname},
-                ${iden.dob},
-                ${iden.gender.toLowerCase()},
-                ${"'patient'"},
-                ${iden.lname},
+                '${iden.fname}',
+                '${iden.dob}',
+                '${iden.gender.toLowerCase()}',
+                '${"patient"}',
+                '${iden.lname}',
                 ${1},
-                ${iden.email?iden.email:null}
+                ${iden.email?iden.email.toString():null}
             )`,{
                 type:db.sequelize.QueryTypes.INSERT,
                 transaction:t
             }
-        ).then(result=>result)
+        ).then(result=>result[0])
 
-        console.log(newUser)
         return newUser
     }catch(err){throw new Error(err)}
+}
+
+createDemandTransaction=async(identification, medPersonnel, t)=>{
+    let userExist = await dbUser(identification, t)
+    let medExist = await dbMedPersonnel(medPersonnel, t)
 }
 
 exports.createTextDemand=async(req, res, next)=>{
@@ -72,10 +105,10 @@ exports.createTextDemand=async(req, res, next)=>{
             entryMethod
         } = req.body
 
-        const userExist = await verifyUserExist(identification,t)
+        const createExists = createDemandTransaction(identification, medPersonnel, t)
         t.commit()
         t.afterCommit(()=>{
-            res.send(userExist)
+            res.json({userId: userExist})
         })
     }catch(err){return next(err)}
 }
